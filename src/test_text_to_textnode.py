daca572cfd6d062d53647c8fcb952f60cbf22ead
Akeem <asbirkett@yahoo.ca>
2025-05-09 00:09:51 -0400

    Added text to textnodes function. Refactored split nodes delimiter code. Created test file for new function

---
+++ src/test_text_to_textnode.py
@@ -0,0 +1,91 @@
+import unittest
+
+from textnode import TextNode, TextType
+from textnode_parser_helper import text_to_textnodes
+
+class Test_TextToTextNode(unittest.TestCase):
+    def test_mixed_text_to_nodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev")
+            ],
+            nodes
+        )
+    
+    def test_mixed_text_to_nodes_multiple_images(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev")
+            ],
+            nodes
+        )
+
+    def test_mixed_text_to_nodes_multiple_links(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) with a link to [youtube](https://www.youtube.com) and another [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" with a link to ", TextType.TEXT),
+                TextNode("youtube", TextType.LINK, "https://www.youtube.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev")
+            ],
+            nodes
+        )
+
+    def test_mixed_text_to_nodes_with_multiple_bolds(self):
+        text = "This is **text** with an _italic_ word with another **bold** and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word with another ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev")
+            ],
+            nodes
+        )
--- src/textnode_parser_helper.py
+++ src/textnode_parser_helper.py
@@ -2,30 +2,32 @@ from textnode import TextNode, TextType
 from leafnode import LeafNode
 import re
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list[TextNode]:
-    nodes = []
-    current_node_text = ""
-    for node in old_nodes:
-        split_by_space_string = node.text.split()
-        for string in split_by_space_string:
-            if string.startswith(delimiter):
-                if len(current_node_text) > 0:
-                    nodes.append(TextNode(current_node_text.strip(), TextType.TEXT))
-                current_node_text = ""
-            elif string.endswith(delimiter):
-                current_node_text += f"{string} "
-                nodes.append(TextNode(current_node_text.strip(), text_type))
-                current_node_text = ""
-                continue
-            
-            current_node_text += f"{string} "
+def __make_delimiter_regex_safe(delimiter: str) -> str:
+    match delimiter:
+        case "**":
+            return "\\*\\*"
+        case _:
+            return delimiter
 
-        if len(current_node_text) > 0:
-            nodes.append(TextNode(current_node_text.strip(), TextType.TEXT))
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    nodes = []
+    regex_safe_delim = __make_delimiter_regex_safe(delimiter)
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            # might not be the best way to do this, but i'm doing it this way
+            split_nodes = re.split(rf"({regex_safe_delim}.*?{regex_safe_delim})", node.text)
+            for string in split_nodes:
+                if string.startswith(delimiter):
+                    nodes.append(TextNode(string.replace(delimiter, ''), text_type))
+                else:
+                    if len(string) > 0:
+                        nodes.append(TextNode(string, TextType.TEXT))
+        else:
+            nodes.append(node)
 
     return nodes
 
-def text_node_to_html_node(text_node):
+def text_node_to_html_node(text_node: TextNode):
     match text_node.text_type:
         case TextType.TEXT:
             return LeafNode(None, text_node.text)
@@ -42,7 +44,7 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception("Invalid text node type")
 
-def extract_markdown_images(text) -> list[tuple]:
+def extract_markdown_images(text: str) -> list[tuple]:
     extract_tuple_list = []
 
     potential_images = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
@@ -52,7 +54,7 @@ def extract_markdown_images(text) -> list[tuple]:
 
     return extract_tuple_list
 
-def extract_markdown_links(text) -> list[tuple]:
+def extract_markdown_links(text: str) -> list[tuple]:
     extract_tuple_list = []
 
     potential_links = re.findall(r"\[(.*?)\]\((.*?)\)", text)
@@ -62,32 +64,51 @@ def extract_markdown_links(text) -> list[tuple]:
 
     return extract_tuple_list
 
-def split_nodes_image(old_nodes):
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     text_nodes = []
 
     for node in old_nodes:
-        split_nodes = re.split(r"(!\[.*?\]\(.*?\))", node.text)
-        for split_node in split_nodes:
-            potential_image_extract = extract_markdown_images(split_node)
-            if len(potential_image_extract) > 0:
-                text_nodes.append(TextNode(potential_image_extract[0][0], TextType.IMAGE, potential_image_extract[0][1]))
-            else:
-                if len(split_node) > 0:
-                    text_nodes.append(TextNode(split_node, TextType.TEXT))            
+        if node.text_type == TextType.TEXT:
+            split_nodes = re.split(r"(!\[.*?\]\(.*?\))", node.text)
+            for split_node in split_nodes:
+                potential_image_extract = extract_markdown_images(split_node)
+                if len(potential_image_extract) > 0:
+                    text_nodes.append(TextNode(potential_image_extract[0][0], TextType.IMAGE, potential_image_extract[0][1]))
+                else:
+                    if len(split_node) > 0:
+                        text_nodes.append(TextNode(split_node, TextType.TEXT))
+        else:
+            text_nodes.append(node)   
 
     return text_nodes
 
-def split_nodes_link(old_nodes):
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     text_nodes = []
 
     for node in old_nodes:
-        split_nodes = re.split(r"(\[.*?\]\(.*?\))", node.text)
-        for split_node in split_nodes:
-            potential_link_extract = extract_markdown_links(split_node)
-            if len(potential_link_extract) > 0:
-                text_nodes.append(TextNode(potential_link_extract[0][0], TextType.LINK, potential_link_extract[0][1]))
-            else:
-                if len(split_node) > 0:
-                    text_nodes.append(TextNode(split_node, TextType.TEXT))            
+        if node.text_type == TextType.TEXT:
+            split_nodes = re.split(r"(\[.*?\]\(.*?\))", node.text)
+            for split_node in split_nodes:
+                potential_link_extract = extract_markdown_links(split_node)
+                if len(potential_link_extract) > 0:
+                    text_nodes.append(TextNode(potential_link_extract[0][0], TextType.LINK, potential_link_extract[0][1]))
+                else:
+                    if len(split_node) > 0:
+                        text_nodes.append(TextNode(split_node, TextType.TEXT))
+        else:
+            text_nodes.append(node)            
 
     return text_nodes
+
+def text_to_textnodes(text: str):
+    node = TextNode(text, TextType.TEXT)
+    final_nodes = [node]
+    final_nodes = split_nodes_delimiter(final_nodes, "`", TextType.CODE)
+    final_nodes = split_nodes_delimiter(final_nodes, "**", TextType.BOLD)
+    final_nodes = split_nodes_delimiter(final_nodes, "_", TextType.ITALIC)
+    final_nodes = split_nodes_image(final_nodes)
+    final_nodes = split_nodes_link(final_nodes)
+    return final_nodes
+
+
+
